db97dbfb35ab4feb76d4460e8d8de20c6769d6e5
german.garcia <german.garcia@tars.dev>
2020-01-29 11:02:43 -0500

    Terminando de detallar nuestro player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import NotFound from '../containers/NotFound';
 import '../assets/styles/components/Player.scss';
 
 const Player = props => {
-  const { id } = props.match.params; 
-  return (
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+  return hasPlaying ? (
     <div className="Player">
       <video controls autoPLay>
-        <source src="" type="video/mp4" />
+        <source src={props.playing.source} type="video/mp4" />
       </video>
       <div className="Player-back">
         <button type="button" onClick={() => props.history.goBack()}>
@@ -14,7 +21,17 @@ const Player = props => {
         </button>
       </div>
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = state => {
+  return {
+    playing: state.playing,
+  }
+}
+
+const mapDispatchToProps = {
+  getVideoSource,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
